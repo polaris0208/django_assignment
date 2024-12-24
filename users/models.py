71f4eaf4d2633309ff7dfe44171e4f1858706516
polaris0208 <sh6771@gmail.com>
2024-12-24 16:28:20 +0900

    feature : 기본 설정 개선 적용
    # 언어설정 변경
    - 한국어
    
    # 유저 모델 개선
    - 프로필 이미지 추가
    - 기본 프로필 이미지 추가
    - 팔로우 계산 로직 추가
    
    # 기본 페이지 개선
    - 링크 횡으로 배치

--- users/models.py
+++ users/models.py
@@ -1,8 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
+
+def user_profile_image_path(instance, filename):
+    return f"profile/{instance.username}/{filename}"
+
 
 class User(AbstractUser):
-  following = models.ManyToManyField(
-    "self", related_name='followers', symmetrical=False
-    ) # symmetrical 대칭 기능
+    profile_image = models.ImageField(
+        default='profile/default.png',
+        upload_to=user_profile_image_path, blank=True, null=True
+    )
+    following = models.ManyToManyField(
+        "self",
+        related_name="followers",
+        symmetrical=False,
+        blank=True,
+    )  # symmetrical 대칭 기능
+
+    @property
+    def follower_counter(self):
+        return self.followers.count()  # 역참조 해서 확인 / 나를 팔로우
+
+    @property
+    def following_counter(self):
+        return self.following.count()  # 정참조 해서 확인 / 내가 팔로우
+
+    def __str__(self):
+        return self.username
