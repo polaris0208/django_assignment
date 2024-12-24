9b706c5b0d5ae9161341ba7651243b795239878c
polaris0208 <sh6771@gmail.com>
2024-12-24 16:26:54 +0900

    [#13] feature : 태그링크 기능 개선
    게시물 내용에서 접근하는 방식에서 해쉬태그에서 접근

--- products/templatetags/make_link.py
+++ products/templatetags/make_link.py
@@ -2,14 +2,7 @@ from django import template
 
 register = template.Library()
 
-
 @register.filter
-def hashtag_link(word):
-    content = word.content + " "
-    hashtags = word.hashtags.all()
-    for hashtag in hashtags:
-        content = content.replace(
-            hashtag.content + " ",
-            f'<a href="/products/{hashtag.pk}/hashtag/">{hashtag.content}</a> ',
-        )
-    return content
+def hashtag_link(hashtag):
+    tag_link = f'<a href="/products/{hashtag.pk}/hashtag/">{hashtag.name}</a>'
+    return tag_link
